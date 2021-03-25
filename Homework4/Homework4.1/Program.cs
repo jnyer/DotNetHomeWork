85dff7c545a194208a7043632e359c3aff384c66
jnyer <864713136@qq.com>
2021-03-25 13:26:09 +0800

    the fourth homework

---
+++ Homework4/Homework4.1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4._1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GerericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GerericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> p = Head;
+            if (p == null) throw new Exception("Error:The List is empty!");
+            while (p!=null)
+            {
+                action(p.Data);
+                p = p.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerericList<int> testList = new GerericList<int>();
+            testList.Add(100);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(10);
+            testList.ForEach(item => Console.Write($"{item},"));
+            int max = 0;
+            testList.ForEach(item => { if (item >= max) max = item; });
+            Console.WriteLine($"\nMax={max}");
+            int min = testList.Head.Data;
+            testList.ForEach(item => { if (item <= min) min = item; });
+            Console.WriteLine($"Min={min}");
+            int sum = 0;
+            testList.ForEach(item => sum += item);
+            Console.WriteLine($"Sum={sum}");
+        }
+    }
+}
---
+++ Homework4/Homework4.2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4._2
+{
+    public class TimeArgs
+    {
+        public int Second { get; set; }
+        public int Minute { get; set; }
+        public int Hour { get; set; }
+    }
+    public delegate void TickHandler(object sender,TimeArgs args);
+    public class ClockFunction
+    {
+        private int hour, minute, second, alarmHour, alarmMinute, alarmSecond;
+        public event TickHandler OnTick;
+        public event TickHandler OnAlarm;
+        public ClockFunction(int hour, int minute,int second)  
+        {
+            if (hour < 60 && minute < 60&& second < 60)
+            {
+                this.hour = hour;
+                this.minute = minute;
+                this.second = second;
+            }
+            else
+            {
+                throw new Exception("error:Time setting is illegal!");
+            }
+        }
+        public void SetAlarm(int aHour,int aMinute,int aSecond)
+        {
+            if (aHour < 60 && aMinute < 60 && aSecond < 60)
+            {
+                this.alarmHour = aHour;
+                this.alarmMinute = aMinute;
+                this.alarmSecond = aSecond;
+            }
+            else
+            {
+                throw new Exception("error:Alarm setting is illegal!");
+            }
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                if (this.second < 59) { this.second++; }
+                else
+                {
+                    this.second = 0;
+                    this.minute++;
+                    if (this.minute == 60)
+                    {
+                        this.minute = 0;
+                        this.hour = (this.hour + 1) % 24;
+                    }
+                }
+                TimeArgs currentTime = new TimeArgs() { Hour = hour, Minute = minute, Second = second };
+                if (this.hour == this.alarmHour && this.minute == this.alarmMinute && this.second==this.alarmSecond)
+                {
+                    OnAlarm(this, currentTime);
+                }
+                else
+                {
+                    OnTick(this, currentTime);
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+    }
+    class Clock
+    {
+        public ClockFunction clock;
+        public Clock(int hour,int minute,int second)
+        {
+            clock = new ClockFunction(hour, minute, second);
+            clock.OnTick += Clock_OnTick;
+            clock.OnAlarm += Clock_OnAlarm;
+        }
+
+        private void Clock_OnTick(object sender, TimeArgs args)
+        {
+            Console.WriteLine($"Current time:{args.Hour}:{args.Minute}:{args.Second}");
+        }
+        private void Clock_OnAlarm(object sender, TimeArgs args)
+        {
+            Console.WriteLine($"Clock is alarming! Time:{args.Hour}:{args.Minute}:{args.Second}");
+        }
+
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock(6, 30, 30);
+            myClock.clock.SetAlarm(6, 30, 40);
+            myClock.clock.Run();
+        }
+    }
+}
