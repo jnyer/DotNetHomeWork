5229679ea47a9c7d37f117fb91f92b45d49ba587
jnyer <864713136@qq.com>
2021-05-20 22:02:26 +0800

    修改

--- Homework12/Startup.cs
+++ Homework12/Startup.cs
@@ -26,30 +26,28 @@ namespace OrderApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services){
-            //services中存放了一组对象，包括DbContext对象、Controller对象等,用于依赖注入
 
              services.AddDbContextPool<OrderContext>(options => options
-                // 连接字符串"todoDatabase" 可以在appsetting.json中设置
                 .UseMySql(Configuration.GetConnectionString("OrderDatabase"), 
                     mySqlOptions => mySqlOptions
                     .ServerVersion(new Version(5, 7, 30), ServerType.MySql)
             ));
-            services.AddControllers(); //创建控制器对象，创建时进行依赖注入
+            services.AddControllers(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env){
-            if (env.IsDevelopment()){ //机器的环境设置为Development
+            if (env.IsDevelopment()){ 
                 app.UseDeveloperExceptionPage(); 
             }
-            app.UseDefaultFiles(); //设置缺省静态文件（index.html或index.htm）
-            app.UseStaticFiles(); //启动静态文件（页面、js、图片等各种前端文件）
+            app.UseDefaultFiles(); 
+            app.UseStaticFiles();
 
-            app.UseHttpsRedirection(); //启动http到https的重定向
-            app.UseRouting();  //将路由组件添加到app中
-            app.UseAuthorization(); //将授权组件添加到app中
+            app.UseHttpsRedirection(); 
+            app.UseRouting();  
+            app.UseAuthorization(); 
             app.UseEndpoints(endpoints =>{
-                endpoints.MapControllers(); //启动映射，将url路由到控制器
+                endpoints.MapControllers(); 
             });            
         }
     }
